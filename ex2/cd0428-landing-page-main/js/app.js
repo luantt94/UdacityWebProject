2c4934ff9e55aaa02dc716f2e86b25ada6c11987
luantt94 <luantt94.uit@gmail.com>
2023-08-30 14:47:51 +0700

    add active css for nav-item, add go to top button, add collapsible

--- ex2/cd0428-landing-page-main/js/app.js
+++ ex2/cd0428-landing-page-main/js/app.js
@@ -53,6 +53,7 @@ function buildNavigationMenu() {
 
     link.textContent = title;
     link.setAttribute("href", `#${id}`);
+    link.setAttribute("class", "nav-item");
 
     menuItem.appendChild(link);
     container.appendChild(menuItem);
@@ -61,16 +62,22 @@ function buildNavigationMenu() {
 
 // Add class 'active' to section when near top of viewport
 function makeActive() {
+  // Get all the navigation items
+  const navItems = document.querySelectorAll(".nav-item");
+
+  // Get all the section
   const sections = document.querySelectorAll("section");
   const threshold = 150;
 
-  sections.forEach((section) => {
+  sections.forEach((section, index) => {
     const box = section.getBoundingClientRect();
 
     if (box.top <= threshold && box.bottom >= threshold) {
       section.classList.add("active");
+      navItems[index].classList.add("active");
     } else {
       section.classList.remove("active");
+      navItems[index].classList.remove("active");
     }
   });
 }
@@ -78,4 +85,34 @@ function makeActive() {
 // Scroll to anchor ID using scrollTO event
 document.addEventListener("scroll", function () {
   makeActive();
+});
+
+// Get all the collapsible headings
+const collapsibleHeadings = document.querySelectorAll(".collapsible");
+
+// Add click event listener to each heading
+collapsibleHeadings.forEach((heading) => {
+  heading.addEventListener("click", function () {
+    // Toggle the display of the content
+    const content = this.nextElementSibling;
+    content.style.display = content.style.display === "none" ? "block" : "none";
+  });
+});
+
+//Add a scroll to top button on the page that’s only visible when the user scrolls below the fold of the page.
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > window.innerHeight) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+});
+
+scrollToTopBtn.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 });
