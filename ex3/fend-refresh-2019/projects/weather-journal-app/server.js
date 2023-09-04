4ddc4d1742f69008df8a8c8371b020aabec60327
luantt94 <luantt94.uit@gmail.com>
2023-09-05 00:06:55 +0700

    finished

--- ex3/fend-refresh-2019/projects/weather-journal-app/server.js
+++ ex3/fend-refresh-2019/projects/weather-journal-app/server.js
@@ -2,18 +2,47 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require("express");
 
 // Start up an instance of app
+const app = express();
+app.use(express.static("public"));
+
+// Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Cors for cross origin allowance
 
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static("website"));
 
+// GET route to return the projectData object
+app.get("/data", (req, res) => {
+  res.send(projectData);
+});
+
+// POST route to add incoming data to projectData
+app.post("/data", (req, res) => {
+  // Assuming the incoming data is in the request body
+  const newData = req.body;
+
+  // Add the new data to the projectData object
+  projectData = {
+    ...projectData,
+    ...newData,
+  };
+
+  res.send("Data added successfully!");
+});
 
 // Setup Server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
--- ex3/fend-refresh-2019/projects/weather-journal-app/website/app.js
+++ ex3/fend-refresh-2019/projects/weather-journal-app/website/app.js
@@ -1,5 +1,60 @@
 /* Global Variables */
+const apiKey = "a120480d1813e997c3f29da55422805e";
+const baseUrl = "https://api.openweathermap.org/data/2.5/weather";
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+async function getWeatherData(zipCode) {
+  const url = `${baseUrl}?zip=${zipCode}&appid=${apiKey}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log("Error:", error);
+  }
+}
+
+$("#generate").click(function () {
+  zipCode = $("#zip").val();
+  feelings = $("#feelings").val();
+  // Create a new date instance dynamically with JS
+  let d = new Date();
+  let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+  getWeatherData(zipCode).then(function (data) {
+    // alert(data.main.temp);
+    const temp = data.main.temp;
+    postData("/data", { temp, feelings, date: newDate });
+    updateUI();
+  });
+});
+
+async function postData(url, data) {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  try {
+    const newData = await response.json();
+    return newData;
+  } catch (error) {
+    console.log("Error:", error);
+  }
+}
+
+async function updateUI() {
+  const response = await fetch("/data");
+  try {
+    const data = await response.json();
+    // Select the necessary elements on the DOM and update their values
+    $("#date").text(`date: ${data.date}`);
+    $("#temp").text(`Temperature: ${data.temp}`);
+    $("#content").text(`Feels: ${data.feelings}`);
+    // alert(s);
+  } catch (error) {
+    console.log("Error:", error);
+  }
+}
