bd83854568b30bbaf84a8f887f709263b2c58003
luantt94 <luantt94.uit@gmail.com>
2023-12-29 09:38:43 +0700

    init db migration

--- javascript/Storefront Backend 2/src/server.ts
+++ javascript/Storefront Backend 2/src/server.ts
@@ -1,15 +1,20 @@
-import express, { Request, Response } from 'express'
-import bodyParser from 'body-parser'
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+import client from "./database";
 
-const app: express.Application = express()
-const address: string = "0.0.0.0:3000"
+const app: express.Application = express();
+const address: string = "0.0.0.0:3000";
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get('/', function (req: Request, res: Response) {
-    res.send('Hello World!')
-})
+app.get("/", function (req: Request, res: Response) {
+  res.send("Hello World!");
+});
 
-app.listen(3000, function () {
-    console.log(`starting app on: ${address}`)
-})
+app.listen(3000, async function () {
+  console.log(`starting app on: ${address}`);
+  const con = await client.connect();
+
+  con.release();
+  console.log("done!");
+});
